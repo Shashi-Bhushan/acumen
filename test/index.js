d0e335b0579f2567ed56a0009cd839115c734b89
Shashi Bhushan <sbhushan1@outlook.com>
2017-07-23 16:31:07 +0530

    Acumen - v0.0.2

--- test/index.js
+++ test/index.js
@@ -3,13 +3,12 @@
 const Acumen = require('..');
 const assert = require('assert');
 
-//var Acumen = require('acumen');
-
 /**
  * Tests
  */
 
 describe('tests()',function(){
+    const alphabets = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
     it('sample test', function() {
         const acumen = new Acumen();
@@ -44,14 +43,25 @@ describe('tests()',function(){
             '#######'
         )
 
+        const d = character(
+            '#######' +
+            '.#....#' +
+            '.#....#' +
+            '.#....#' +
+            '.#....#' +
+            '.#....#' +
+            '#######'
+        )
+
         /**
          * Learn the letters A through C.
          */
 
         acumen.learn([
-                { input: a, output: map('a') },
-                { input: b, output: map('b') },
-                { input: c, output: map('c') }
+                { input: a, output: map('a') }, // 0.0
+                { input: b, output: map('b') }, // 0.2
+                { input: c, output: map('c') }, // 0.4
+                { input: d, output: map('d') }  // 0.6
             ])
 
         /**
@@ -68,8 +78,8 @@ describe('tests()',function(){
         );
         const result = acumen.predict(resultChar);
 
-        console.log("Character Test Expected Result is : " + map('c')) // ~ 0.5
-        console.log("Character Test Result is : " + result[0]) // ~ 0.5
+        console.log("Character Test Expected Result is : " + map('c')) // ~ 0.6
+        console.log("Character Test Result is : " + result[0]) // ~ 0.6
     })
 
     it('XOR Test', function() {
@@ -101,9 +111,6 @@ describe('tests()',function(){
      */
 
     function map(letter) {
-        if (letter === 'a') return [ 0.1 ]
-        if (letter === 'b') return [ 0.3 ]
-        if (letter === 'c') return [ 0.5 ]
-        return 0
+        return [ alphabets.indexOf(letter)/10 * 2 ];
     }
 });
